2d4ad4dbb3c02a9d0cdc6a3dfbe32a0c3db13f86
shakun1990 <er.shakun90@gmail.com>
2020-08-29 21:26:39 +0530

    header added

--- src/stories/SimpleTable.js
+++ src/stories/SimpleTable.js
@@ -1,45 +1,55 @@
-import React, {useState, useEffect} from 'react';
-import { withStyles, makeStyles, TableRow, TableHead, TableContainer, TableCell, TableBody, Modal, Table, Paper } from '@material-ui/core';
-import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
-import MomentUtils from '@date-io/moment';
-import moment from 'moment';
+import React, { useState, useEffect } from "react";
+import {
+  withStyles,
+  makeStyles,
+  TableRow,
+  TableHead,
+  TableContainer,
+  TableCell,
+  TableBody,
+  Modal,
+  Table,
+  Paper,
+} from "@material-ui/core";
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import MomentUtils from "@date-io/moment";
+import moment from "moment";
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
-    backgroundColor: '#F1F1F4',
-    color: '#556789',
-    fontWeight: 'bold',
-    textTransform: 'uppercase'
+    backgroundColor: "#F1F1F4",
+    color: "#556789",
+    fontWeight: "bold",
+    textTransform: "uppercase",
   },
   body: {
     fontSize: 14,
   },
 }))(TableCell);
 
-
 const useStyles = makeStyles((theme) => ({
   table: {
     minWidth: 700,
   },
   paper: {
-    position: 'absolute',
+    position: "absolute",
     width: 400,
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
+    border: "2px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
 }));
 
 const datePickerProperties = {
-  views: ['date'],
-  orientation : 'landscape',
-  variant: 'inline',
-  isToolbarEnabled : false,
-  inputProps : {disableunderline : "true"},
-  format: 'M/D/YYYY',
-  isAutoOkEnabled : true
-}
+  views: ["date"],
+  orientation: "landscape",
+  variant: "inline",
+  isToolbarEnabled: false,
+  inputProps: { disableunderline: "true" },
+  format: "M/D/YYYY",
+  isAutoOkEnabled: true,
+};
 
 function rand() {
   return Math.round(Math.random() * 20) - 10;
@@ -62,10 +72,10 @@ export default function SimpleTable(props) {
   const [isOpen, setIsOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalStyle] = React.useState(getModalStyle);
-  const [info,setInfo] = useState({ fieldname:'', fieldvalue:'' });
+  const [info, setInfo] = useState({ fieldname: "", fieldvalue: "" });
   useEffect(() => {
     setRowData(props.rowsData);
-  }, [props.rowsData])  
+  }, [props.rowsData]);
 
   const handleOpen = () => {
     setIsModalOpen(true);
@@ -74,30 +84,44 @@ export default function SimpleTable(props) {
     setIsModalOpen(false);
   };
 
-  const handleDateChange = (dateName, dateValue) => {   
-    setInfo({...info, fieldname: dateName, fieldvalue: dateValue })
-  }
-  
+  const handleDateChange = (dateName, dateValue) => {
+    setInfo({ ...info, fieldname: dateName, fieldvalue: dateValue });
+  };
 
   const body = (campaignName, region, thumbnail) => {
     return (
-    <div style={modalStyle} className={classes.paper}>
-      <span style={{float: 'left', paddingRight : '8px'}}>{<img src={require(`./assets/${thumbnail}`)}  className="img-responsive" />}</span>
-      <span>{campaignName} <br/> {region}</span>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <h2 id="simple-modal-title"><b>Pricing</b></h2>
-      <span id="simple-modal-description">
-         1 Week - 1 Month - $100<br/>
-         6 Month - $500<br/>
-         1 Year  - $900<br/>
-      </span>
-      <button type="button" onClick={handleClose}>
-        Close
-      </button>
-    </div>
+      <div style={modalStyle} className={classes.paper}>
+        <span style={{ float: "left", paddingRight: "8px" }}>
+          {
+            <img
+              alt="thumbnail"
+              src={require(`./assets/${thumbnail}`)}
+              className="img-responsive"
+            />
+          }
+        </span>
+        <span>
+          {campaignName} <br /> {region}
+        </span>
+        <br />
+        <br />
+        <br />
+        <br />
+        <h2 id="simple-modal-title">
+          <b>Pricing</b>
+        </h2>
+        <span id="simple-modal-description">
+          1 Week - 1 Month - $100
+          <br />
+          6 Month - $500
+          <br />
+          1 Year - $900
+          <br />
+        </span>
+        <button type="button" onClick={handleClose}>
+          Close
+        </button>
+      </div>
     );
   };
 
@@ -105,83 +129,152 @@ export default function SimpleTable(props) {
     let status;
     switch (props.timeperiod) {
       case "past":
-        status = moment().diff(date, 'days') > 0
-      break;
+        status = moment().diff(date, "days") > 0;
+        break;
       case "present":
-        status = moment().diff(date, 'days') === 0
-      break;
+        status = moment().diff(date, "days") === 0;
+        break;
       default:
-        status = moment().diff(date, 'days') < 0
-      break;
+        status = moment().diff(date, "days") < 0;
+        break;
     }
-    return status;    
-  }
+    return status;
+  };
 
   return (
     <TableContainer component={Paper}>
       <Table className={classes.table} aria-label="customized table">
         <TableHead>
           <TableRow>
-            <StyledTableCell>{props.t ? props.t('Date') : 'Date'}</StyledTableCell>
-            <StyledTableCell>{props.t ? props.t('Campaign') : 'Campaign'}</StyledTableCell>
-            <StyledTableCell>{props.t ? props.t('View') : 'View'}</StyledTableCell>
-            <StyledTableCell>{props.t ? props.t('Actions') : 'Actions'}</StyledTableCell>
+            <StyledTableCell>
+              {props.t ? props.t("Date") : "Date"}
+            </StyledTableCell>
+            <StyledTableCell>
+              {props.t ? props.t("Campaign") : "Campaign"}
+            </StyledTableCell>
+            <StyledTableCell>
+              {props.t ? props.t("View") : "View"}
+            </StyledTableCell>
+            <StyledTableCell>
+              {props.t ? props.t("Actions") : "Actions"}
+            </StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {Array.from(rowData.values())
-              .filter((row) => timeperiod(row.date))
-              .map((row) =>               
-              <TableRow key={'row-'+ row.rowId}>
-                  <StyledTableCell style={{whiteSpace: 'pre-line'}}>{moment(row.date).format('MMM Y, D')}<br/><i>{moment(row.date).fromNow()}</i></StyledTableCell>
-                  <StyledTableCell>
-                    <span style={{float: 'left', paddingRight : '8px'}}>{<img width="50px" src={require(`./assets/${row.thumbnail}`)}  className="img-responsive" />}</span>
-                    <span>{row.campaignName} <br/> <i>{row.region}</i></span>
-                  </StyledTableCell>
-                  <StyledTableCell> 
-                    <span style={{paddingRight : '30px', cursor : 'pointer'}} onClick={handleOpen.bind(this)}>
-                        <span style={{ float: 'left', paddingRight : '8px'}}>
-                          {<img width="35px" src={require(`./assets/Price.png`)} className="img-responsive" />}
-                        </span>
-                        View Pricing
-                    </span> 
-                    <Modal
-                        id={'price-modal-'+ row.rowId}
-                        open={isModalOpen}
-                        onClose={handleClose}
-                        aria-labelledby="simple-modal-title"
-                        aria-describedby="simple-modal-description"
-                      >
+            .filter((row) => timeperiod(row.date))
+            .map((row) => (
+              <TableRow key={"row-" + row.rowId}>
+                <StyledTableCell style={{ whiteSpace: "pre-line" }}>
+                  {moment(row.date).format("MMM Y, D")}
+                  <br />
+                  <i>{moment(row.date).fromNow()}</i>
+                </StyledTableCell>
+                <StyledTableCell>
+                  <span style={{ float: "left", paddingRight: "8px" }}>
+                    {
+                      <img
+                        alt="thumbnail"
+                        width="50px"
+                        src={require(`./assets/${row.thumbnail}`)}
+                        className="img-responsive"
+                      />
+                    }
+                  </span>
+                  <span>
+                    {row.campaignName} <br /> <i>{row.region}</i>
+                  </span>
+                </StyledTableCell>
+                <StyledTableCell>
+                  <span
+                    style={{ paddingRight: "30px", cursor: "pointer" }}
+                    onClick={handleOpen.bind(this)}
+                  >
+                    <span style={{ float: "left", paddingRight: "8px" }}>
+                      {
+                        <img
+                          alt="viewpricing"
+                          width="35px"
+                          src={require(`./assets/Price.png`)}
+                          className="img-responsive"
+                        />
+                      }
+                    </span>
+                    View Pricing
+                  </span>
+                  <Modal
+                    id={"price-modal-" + row.rowId}
+                    open={isModalOpen}
+                    onClose={handleClose}
+                    aria-labelledby="simple-modal-title"
+                    aria-describedby="simple-modal-description"
+                  >
                     {body(row.campaignName, row.region, row.thumbnail)}
                   </Modal>
-                  </StyledTableCell>
-                  <StyledTableCell>
-                  <span style={{paddingRight : '35px'}}>
-                    <span style={{float: 'left', paddingRight : '8px'}}>
-                      {<img width="30px" src={require(`./assets/file.png`)} className="img-responsive" />}
+                </StyledTableCell>
+                <StyledTableCell>
+                  <span style={{ paddingRight: "35px" }}>
+                    <span style={{ float: "left", paddingRight: "8px" }}>
+                      {
+                        <img
+                          alt="csv"
+                          width="30px"
+                          src={require(`./assets/file.png`)}
+                          className="img-responsive"
+                        />
+                      }
                     </span>
-                    CSV 
-                  </span> 
+                    CSV
+                  </span>
 
-                  <span style={{paddingRight : '35px'}}>
-                    <span style={{paddingRight : '8px'}}>
-                      {<img width="30px" src={require(`./assets/statistics-report.png`)} className="img-responsive" />}
+                  <span style={{ paddingRight: "35px" }}>
+                    <span style={{ paddingRight: "8px" }}>
+                      {
+                        <img
+                          alt="Report"
+                          width="30px"
+                          src={require(`./assets/statistics-report.png`)}
+                          className="img-responsive"
+                        />
+                      }
                     </span>
-                    Report 
-                  </span> 
+                    Report
+                  </span>
 
-                  <span style={{paddingRight : '35px', cursor : 'pointer'}} onClick={() => setIsOpen(!isOpen)} >
-                    <span style={{paddingRight : '8px'}}>
-                      {<img width="30px" src={require(`./assets/calendar.png`)} className="img-responsive" />}
+                  <span
+                    style={{ paddingRight: "35px", cursor: "pointer" }}
+                    onClick={() => setIsOpen(!isOpen)}
+                  >
+                    <span style={{ paddingRight: "8px" }}>
+                      {
+                        <img
+                          alt="Schedule Again"
+                          width="30px"
+                          src={require(`./assets/calendar.png`)}
+                          className="img-responsive"
+                        />
+                      }
                     </span>
-                    Schedule Again 
-                  </span>               
+                    Schedule Again
+                  </span>
                   <MuiPickersUtilsProvider utils={MomentUtils}>
-                    <DatePicker style={{display : 'none'}} id={'date-picker-'+ row.rowId} inputProps={datePickerProperties.inputProps} disableunderline="true" value={info['date-picker-'+ row.rowId]} onChange={(value) => handleDateChange('date-picker-'+ row.rowId , value)} open={isOpen} onClose={()=> setIsOpen(false)} onOpen={()=> setIsOpen(true)}/>
-                  </MuiPickersUtilsProvider>              
-                  </StyledTableCell>
-            </TableRow>             
-            )}
+                    <DatePicker
+                      style={{ display: "none" }}
+                      id={"date-picker-" + row.rowId}
+                      inputProps={datePickerProperties.inputProps}
+                      disableunderline="true"
+                      value={info["date-picker-" + row.rowId]}
+                      onChange={(value) =>
+                        handleDateChange("date-picker-" + row.rowId, value)
+                      }
+                      open={isOpen}
+                      onClose={() => setIsOpen(false)}
+                      onOpen={() => setIsOpen(true)}
+                    />
+                  </MuiPickersUtilsProvider>
+                </StyledTableCell>
+              </TableRow>
+            ))}
         </TableBody>
       </Table>
     </TableContainer>
--- src/stories/SimpleTabs.js
+++ src/stories/SimpleTabs.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles, Tabs, Tab, Typography, Box, Button, ButtonGroup, AppBar, Toolbar  } from '@material-ui/core';
-import { useTranslation, withTranslation } from 'react-i18next';
-import SimpleTable from './SimpleTable';
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  makeStyles,
+  Tabs,
+  Tab,
+  Typography,
+  Box,
+  AppBar,
+  Toolbar,
+  Menu,
+  MenuItem,
+  IconButton,
+} from "@material-ui/core";
+import { useTranslation, withTranslation } from "react-i18next";
+import { LanguageIcon } from "@material-ui/icons";
+import SimpleTable from "./SimpleTable";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -32,91 +44,162 @@ TabPanel.propTypes = {
 function a11yProps(index) {
   return {
     id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
   };
 }
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1, 
+    flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
   },
   indicator: {
-    backgroundColor: '#83A515',
+    backgroundColor: "#83A515",
   },
-  selectedTab : {
-    color: '#83A515',
-    fontWeight: 'bold',
+  selectedTab: {
+    color: "#83A515",
+    fontWeight: "bold",
   },
-  label: {    
-    fontWeight: 'inherit',
-    fontSize: '16px',
-    textTransform: 'none'
+  label: {
+    fontWeight: "inherit",
+    fontSize: "16px",
+    textTransform: "none",
   },
-  languageToolbar : {
-    float : 'right'
+  languageToolbar: {
+    float: "right",
   },
-  heading : {
-    fontWeight: 'bold',
-    color: '#2B416C',
-    paddingTop: '20px',
-    paddingBottom: '10px',
+  heading: {
+    fontWeight: "bold",
+    color: "#2B416C",
+    paddingTop: "20px",
+    paddingBottom: "10px",
   },
-  header : {
-    backgroundColor: '#1F2640',
+  header: {
+    backgroundColor: "#1F2640",
   },
-  logo : {
-    paddingTop: '10px',
-    paddingBottom: '10px',
-    width : '15%'
+  logo: {
+    paddingTop: "10px",
+    paddingBottom: "10px",
+    width: "15%",
   },
-  logoWrapper : {
-    paddingLeft : '3px'
-  }
+  logoWrapper: {
+    paddingLeft: "3px",
+  },
+  title: {
+    flexGrow: 1,
+  },
 }));
 
 function SimpleTabs(props) {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
   const { t, i18n } = useTranslation();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
 
-  const changeLanguage = lng => {
-     i18n.changeLanguage(lng);
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+    setAnchorEl(null);
   };
 
   const handleChange = (event, newValue) => {
-    setValue(newValue);   
+    setValue(newValue);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
   return (
     <div className={classes.root}>
       <AppBar position="static" className={classes.header}>
-        <Toolbar variant="dense" className={classes.logoWrapper}>          
-           <img className={classes.logo} src="https://cdn-www.bluestacks.com/bs-images/bs-logo-new.png"/> 
-          <Typography variant="h6" color="inherit">            
+        <Toolbar variant="dense" className={classes.logoWrapper}>
+          <Typography variant="h6" className={classes.title}>
+            <img
+              className={classes.logo}
+              alt="logo"
+              src="https://cdn-www.bluestacks.com/bs-images/bs-logo-new.png"
+            />
           </Typography>
+          <IconButton
+            aria-label="language"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
+            color="inherit"
+          >
+            <LanguageIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={open}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={() => changeLanguage("en")}>English</MenuItem>
+            <MenuItem onClick={() => changeLanguage("de")}>German</MenuItem>
+          </Menu>
         </Toolbar>
-      </AppBar> 
-      <Typography variant="h4" gutterBottom className={classes.heading}>        
-        {t ? t('Manage Campaigns') : 'Manage Campaigns'}
+      </AppBar>
+      <Typography variant="h4" gutterBottom className={classes.heading}>
+        {t ? t("Manage Campaigns") : "Manage Campaigns"}
       </Typography>
-      <ButtonGroup className={classes.languageToolbar} color="secondary" aria-label="outlined secondary button group">
-        <Button onClick={() => changeLanguage('en')}>English</Button>
-        <Button onClick={() => changeLanguage('de')}>German</Button>
-      </ButtonGroup>     
-      <Tabs id="bluestack-simpleTabs" classes={{ indicator: classes.indicator }}  value={value} onChange={handleChange} aria-label="simple tabs example">
-        <Tab classes={{ selected: classes.selectedTab }} label={<span className={classes.label}>{t ? t('Upcoming Campaigns') : 'Upcoming Campaigns'}</span>} {...a11yProps(0)} />
-        <Tab classes={{ selected: classes.selectedTab }} label={<span className={classes.label}>{t ? t('Live Campaigns') : 'Live Campaigns'}</span>} {...a11yProps(1)} />
-        <Tab classes={{ selected: classes.selectedTab }} label={<span className={classes.label}>{t ? t('Past Campaigns') : 'Past Campaigns'}</span>} {...a11yProps(2)} />
-      </Tabs>      
-      <TabPanel value={value} index={0}>        
-        <SimpleTable timeperiod="past" t={t} rowsData={props.rowsData}/>
+      <Tabs
+        id="bluestack-simpleTabs"
+        classes={{ indicator: classes.indicator }}
+        value={value}
+        onChange={handleChange}
+        aria-label="simple tabs example"
+      >
+        <Tab
+          classes={{ selected: classes.selectedTab }}
+          label={
+            <span className={classes.label}>
+              {t ? t("Upcoming Campaigns") : "Upcoming Campaigns"}
+            </span>
+          }
+          {...a11yProps(0)}
+        />
+        <Tab
+          classes={{ selected: classes.selectedTab }}
+          label={
+            <span className={classes.label}>
+              {t ? t("Live Campaigns") : "Live Campaigns"}
+            </span>
+          }
+          {...a11yProps(1)}
+        />
+        <Tab
+          classes={{ selected: classes.selectedTab }}
+          label={
+            <span className={classes.label}>
+              {t ? t("Past Campaigns") : "Past Campaigns"}
+            </span>
+          }
+          {...a11yProps(2)}
+        />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <SimpleTable timeperiod="past" t={t} rowsData={props.rowsData} />
       </TabPanel>
       <TabPanel value={value} index={1}>
-        <SimpleTable timeperiod="present" t={t} rowsData={props.rowsData}/>
+        <SimpleTable timeperiod="present" t={t} rowsData={props.rowsData} />
       </TabPanel>
       <TabPanel value={value} index={2}>
-        <SimpleTable timeperiod="future" t={t} rowsData={props.rowsData}/>
+        <SimpleTable timeperiod="future" t={t} rowsData={props.rowsData} />
       </TabPanel>
     </div>
   );
